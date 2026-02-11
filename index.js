1bdb7b23507b3d1691ae741477acb59bfbf423ea
victormelladobuvinic-cpu <victor.melladobuvinic@gmail.com>
2026-02-10 21:02:36 -0300

    js render from API v2

--- index.js
+++ index.js
@@ -37,16 +37,24 @@ function getMovieDataSearch(key, title) {
                 let movieArray = []
                 movieArray.push(movieData)
 
-                let htmlMovies = movieArray.map(movie => {
+                let htmlMovies = movieArray.map(({imdbID, Poster, Title, imdbRating, Runtime, Genre, Plot}) => {
                     return `
-                    <div class="movie-card" id ="${movie.imdbID}">
-                        <img src="${movie.Poster}" alt="${movie.Title} poster">
-                        <h2>${movie.Title}</h2>
-                        <p>Release Year: ${movie.Year}</p>
-                        <p>Genre: ${movie.Genre}</p>
-                        <p>Director: ${movie.Director}</p>
-                        <p>Plot: ${movie.Plot}</p>
-                    </div>
+                   
+                        <div class="movie-card" data-imdbid="${imdbID}">
+                            <img src="${Poster}" alt="${Title} poster" class="movie-poster">
+                            <div class="info-container">
+                                <div class="movie-details">
+                                    <h2 class="movie-title">${Title}</h2>
+                                    <p class="movie-rating"><i class="fa-regular fa-star"></i> ${imdbRating}</p>
+                                </div>
+                                <div class="movie-info">
+                                    <p class="movie-runtime">Runtime: ${Runtime}</p>
+                                    <p class="movie-genre">Genre: ${Genre}</p>
+                                    <button class="add-watchlist"><i class="fa-solid fa-circle-plus"></i>Watchlist</button>           
+                                </div>
+                                <p class="movie-plot">${Plot}</p>
+                            </div>
+                        </div>
                     `
                 }).join("")
                 
@@ -61,3 +69,6 @@ function getMovieDataSearch(key, title) {
     })
 
 }
+
+
+
